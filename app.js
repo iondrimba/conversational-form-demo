3d09a34b27ebeb1256456c7139264a14b6d3a08d
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2017-07-07 16:08:11 -0300

    localization UI

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ $(document).ready(function () {
     formEl: document.getElementById('request-form'),
     userImage: 'http://via.placeholder.com/24x24',
     robotImage: 'ninja_avatar.png',
+    dictionaryData: {
+      "input-placeholder": "Digite sua resposta ...",
+      "input-no-filter": "Nenhum resultado encontrado com <strong>{input-value}</strong>",
+      "user-reponse-and": " e "
+    },
     submitCallback: function (e) {
       var formData = firstStep.getFormData();
       var formDataSerialized = firstStep.getFormData(true);
