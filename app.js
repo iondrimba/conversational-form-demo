c42e8bd508cc3dc8f51cb2a8ddf4c1fa88cdcb4a
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2017-07-07 17:41:08 -0300

    fix demo

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 $(document).ready(function () {
-  var f = $('.user').next();
-  var c = $('.user').children();
-  f.append(c);
-
-  var firstStep = new cf.ConversationalForm({
-    formEl: document.getElementById('request-form'),
-    userImage: 'http://via.placeholder.com/24x24',
-    robotImage: 'ninja_avatar.png',
-    preventAutoStart: true,
-    dictionaryData: {
-      "input-placeholder": "Digite sua resposta ...",
-      "input-no-filter": "Nenhum resultado encontrado com <strong>{input-value}</strong>",
-      "user-reponse-and": " e "
-    },
-    submitCallback: function (e) {
-      var formData = firstStep.getFormData();
-      var formDataSerialized = firstStep.getFormData(true);
-      console.log("Formdata:", formData);
-      console.log("Formdata, serialized:", formDataSerialized);
-    }
-  });
-
   $("body").dblclick(function () {
+    var f = $('.user').next();
+    var c = $('.user').children();
+    f.append(c);
+
+    var firstStep = new cf.ConversationalForm({
+      formEl: document.getElementById('request-form'),
+      userImage: 'http://via.placeholder.com/24x24',
+      robotImage: 'ninja_avatar.png',
+      preventAutoStart: true,
+      dictionaryData: {
+        "input-placeholder": "Digite sua resposta ...",
+        "input-no-filter": "Nenhum resultado encontrado com <strong>{input-value}</strong>",
+        "user-reponse-and": " e "
+      },
+      submitCallback: function (e) {
+        var formData = firstStep.getFormData();
+        var formDataSerialized = firstStep.getFormData(true);
+        console.log("Formdata:", formData);
+        console.log("Formdata, serialized:", formDataSerialized);
+      }
+    });
     $('.conversational-form--show').show();
     firstStep.start();
   });
