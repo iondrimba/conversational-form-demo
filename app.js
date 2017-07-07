820f91d5050c970b84e758757d6e9dfa5321ec2b
Ion Drimba Filho <ion.drimba@getninjas.com.br>
2017-07-07 10:03:42 -0300

    sanitized initial files

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  console.log('ready')
+  // new cf.ConversationalForm({
+  //   // HTMLFormElement
+  //   formEl: "#request-form",
+  //   //base64 || image url // overwrite user image, without overwritting the user dictionary
+  //   userImage: "",
+  //   /*?: string;*/
+
+  //   // base64 || image url // overwrite robot image, without overwritting the robot dictionary
+  //   robotImage: "",
+  //   /*?: string;*/
+
+  //   // custom submit callback if button[type=submit] || form.submit() is not wanted..
+  //   submitCallback: function() {
+  //     console.log('submit');
+  //   },
+  //   loadExternalStyleSheet: true,
+  // });
+});
---
+++ browser-sync.js
@@ -0,0 +1,13 @@
+module.exports = {
+  server: {
+    baseDir: "",
+    index: "index.htm"
+  },
+  open: "local",
+  injectChanges: true,
+  startPath: "index.html",
+  watchOptions: {
+    ignoreInitial: true,
+  },
+  files: ['*.html', '*.js']
+}
